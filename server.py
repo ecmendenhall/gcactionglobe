91c6eb38cd9dd3d7562c13be88ca946a353360e1
Connor Mendenhall <cmendenhall@8thlight.com>
2016-05-20 21:20:50 +0000

    Send coordinates over websocket.

--- server.py
+++ server.py
@@ -3,13 +3,20 @@ import logging
 import gevent
 from flask import Flask, render_template, request
 from flask_sockets import Sockets
-
+from geoip import geolite2
 
 app = Flask(__name__)
 app.debug = 'DEBUG' in os.environ
 
 sockets = Sockets(app)
+WEBSOCKETS = []
 
+def get_ip_coordinates(ip_address):
+    address = geolite2.lookup(ip_address)
+    if address is None:
+        return (38.000, -97.000)
+    else:
+        return address.location
 
 @app.route('/')
 def hello():
@@ -18,7 +25,12 @@ def hello():
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
-    print 'webhook'
-    print str(data)
-    print data['context']['ip']
+    ip = data['context']['ip']
+    lat, lon = get_ip_coordinates(ip)
+    for socket in WEBSOCKETS:
+        socket.send({'lat': lat, 'lon': lon})
     return data['context']['ip']
+
+@sockets.route('/receive')
+def receive(websocket):
+    WEBSOCKETS.append(websocket)
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import server
 import unittest
 import json
+from server import get_ip_coordinates
+import mock
 
 class ServerTests(unittest.TestCase):
 
@@ -9,10 +11,13 @@ class ServerTests(unittest.TestCase):
         self.app = server.app.test_client()
         self.context_data = {
             'context': {
-                'ip': '123.45.67.89'
+                'ip': '74.101.51.84'
             }
         }
         self.json = json.dumps(self.context_data)
+
+    def tearDown(self):
+        server.WEBSOCKETS = []
 
     def test_returns_hello_world(self):
         response = self.app.get('/')
@@ -25,6 +30,36 @@ class ServerTests(unittest.TestCase):
             content_type='application/json'
         )
         assert self.context_data['context']['ip'] in str(response.data)
+
+    def test_webhook_notifies_websockets(self):
+        fake_websocket = mock.MagicMock()
+        server.receive(fake_websocket)
+        response = self.app.post(
+            '/webhook',
+            data=self.json,
+            content_type='application/json'
+        )
+        lat_lon = {'lat': 40.9126, 'lon': -73.8371}
+        fake_websocket.send.assert_called_with(lat_lon)
+
+class GeoIPTests(unittest.TestCase):
+
+    def test_returns_lat_lon_for_ip(self):
+        coordinates = get_ip_coordinates('74.101.51.84')
+        self.assertEqual(coordinates, (40.9126, -73.8371))
+
+    @mock.patch('server.geolite2')
+    def test_returns_lat_lon_for_ip(self, geolite2):
+        geolite2.lookup.return_value = None
+        coordinates = get_ip_coordinates('74.101.51.84')
+        self.assertEqual(coordinates, (38.0000,-97.0000))
+
+class WebsocketTests(unittest.TestCase):
+
+    def test_receive_route_adds_socket_to_observers(self):
+        fake_websocket = mock.MagicMock()
+        server.receive(fake_websocket)
+        self.assertEqual(1, len(server.WEBSOCKETS))
 
 if __name__ == '__main__':
     unittest.main()
