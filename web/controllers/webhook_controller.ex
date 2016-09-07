722e91cbd2809389568536c114a590cc82b8d887
Connor Mendenhall <ecmendenhall@gmail.com>
2016-09-07 16:49:08 -0400

    Handle missing IPs

--- web/controllers/webhook_controller.ex
+++ web/controllers/webhook_controller.ex
@@ -4,17 +4,22 @@ defmodule Gcactionglobe.WebhookController do
   def webhook(conn, params) do
     event = params["event"]
     ip = params["context"]["ip"]
-    %{city: city} = Geolix.lookup(ip)
-    latlon = %{
-        ip: ip,
-        lat: city.location.latitude,
-        lon: city.location.longitude
-      }
-    if event == "Event Action Take" do
-      Gcactionglobe.Endpoint.broadcast("activity:action", "new_msg", latlon)
+    IO.inspect(ip)
+    if ip != nil do
+      %{city: city} = Geolix.lookup(ip)
+      latlon = %{
+          ip: ip,
+          lat: city.location.latitude,
+          lon: city.location.longitude
+        }
+      if event == "Event Action Take" do
+        Gcactionglobe.Endpoint.broadcast("activity:action", "new_msg", latlon)
+      end
+      Gcactionglobe.Endpoint.broadcast("activity:*", "new_msg", latlon)
+      json conn, latlon
+    else
+      json conn, %{}
     end
-    Gcactionglobe.Endpoint.broadcast("activity:*", "new_msg", latlon)
-    json conn, latlon
   end
 
 end
